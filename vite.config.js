babbd9b95984b0ae0f73b6bb1feea498c08ff60a
takeshi <kkrakenbreaker@gmail.com>
2022-10-04 22:17:46 -0500

    Add config for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import Precss from 'precss';
 
 export default defineConfig({
+    base: "https://celestial-blink.github.io/tcalculator/",
     css: {
         postcss: {
             plugins: [
