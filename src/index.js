21750d93a9da31852f08abd9f01e28fbca460773
takeshi <kkrakenbreaker@gmail.com>
2022-10-04 23:07:55 -0500

    Fix resize for 350px, Feat automatic loading darkmode

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import './views/calulator/styles.pcss';
 import templateCalculator from './views/calulator/index.html?raw';
 
 //calculator theme event
-import handleTheme from './scripts/theme';
+import { handleTheme, handleStartTheme } from './scripts/theme';
 import handleClick from './scripts/buttons';
 
 const startApp = () => {
     document.getElementById("app").innerHTML = templateCalculator;
     document.getElementById("mode").addEventListener('change', handleTheme);
     document.getElementById("tcalculator__buttons").addEventListener('click', handleClick);
+    handleStartTheme();
 }
 
 document.addEventListener('DOMContentLoaded',()=>{
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -3,10 +3,36 @@ const handleTheme = (event) => {
     const elementCalculator = document.getElementById("tcalculator");
     if (currentTarget.checked) {
         elementCalculator.classList.add("dark-mode");
+        window.localStorage.setItem("isDarkMode", 1);
+    } else {
+        elementCalculator.classList.remove("dark-mode");
+        window.localStorage.setItem("isDarkMode", 0);
+    }
+}
+
+const handleStartTheme = event => {
+    const elementCalculator = document.getElementById("tcalculator");
+    const elementCheckboxTheme = document.getElementById("mode");
+    
+    const getDarkMode = window.localStorage.getItem("isDarkMode");
+
+    let prepareDarkMode = false;
+    if (getDarkMode === null) {
+        const mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
+        const isDarkMode = mediaQueryObj.matches;
+        prepareDarkMode = !!isDarkMode;
+    } else {
+        prepareDarkMode = !!(getDarkMode * 1);
+    }
+
+    if (prepareDarkMode) {
+        elementCalculator.classList.add("dark-mode");
+        elementCheckboxTheme.checked = true;
     } else {
         elementCalculator.classList.remove("dark-mode");
     }
-    
+
 }
 
-export default handleTheme;
+export { handleTheme, handleStartTheme };
+
